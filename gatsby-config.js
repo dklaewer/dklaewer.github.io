6ddbd12ce158bd8b4b7de205fccaf38f5be6564e
Daniel Kläwer <d.klaewer@gmail.com>
2022-12-11 16:59:50 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Daniel Klaewer's personal site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss"]
+};
