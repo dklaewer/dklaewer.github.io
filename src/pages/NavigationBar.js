2c6d1609eab25675cea54a6287213eac2973b39a
Daniel Kläwer <d.klaewer@gmail.com>
2023-01-03 15:30:23 +0100

    smaller menu bar

--- src/pages/NavigationBar.js
+++ src/pages/NavigationBar.js
@@ -8,7 +8,7 @@ export default function NavigationBar(props) {
   }
   return (
     <div className="sticky top-0 z-50">
-      <nav className="flex items-center justify-between flex-wrap bg-slate-500 p-6">
+      <nav className="flex items-center justify-between flex-wrap bg-slate-500 p-4">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
           <span className="font-semibold text-xl tracking-tight">Daniel Kläwer</span>
         </div>
